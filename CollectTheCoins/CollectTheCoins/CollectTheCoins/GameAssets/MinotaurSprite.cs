f41e9615220f0d9c02d1696d712d352920890a1a
Agustin Rodriguez <agustinr@ksu.edu>
2021-11-03 01:38:33 -0500

    Made changes to assets and added minotaur. Need to finish implementation.

---
+++ CollectTheCoins/CollectTheCoins/CollectTheCoins/GameAssets/MinotaurSprite.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectTheCoins.GameAssets
+{
+    /// <summary>
+    /// Enum to handle the direction the minotaur walks in 
+    /// </summary>
+    public enum MinotaurDirection
+    {
+        Up = 0,
+        Right = 1,
+        Down = 2,
+        Left = 3,
+    }
+
+    /// <summary>
+    /// Class to represent a minotaur sprite enemy
+    /// </summary>
+    public class MinotaurSprite
+    {
+
+    }
+}
